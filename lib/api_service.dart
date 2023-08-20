3749f705ffd3b7b438fd7817b1abe25b3e6507c0
Pedro Tabacof <tabacof@gmail.com>
2023-08-20 16:14:15 +0100

    adding mobile API / Python server integration

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -2,11 +2,22 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:device_info/device_info.dart';
 import 'dart:io';
+import 'dart:convert';
 
 class ApiService {
-  final String baseUrl = 'http://0.0.0.0:8080/';
+  final String baseUrl = 'http://0.0.0.0:8080';
 
   Future<String> getDeviceId() async {
+    bool isTest = false;
+    assert(() {
+      isTest = true;
+      return true;
+    }());
+
+    if (isTest) {
+      return 'testDeviceId';
+    }
+
     DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
     if (Platform.isAndroid) {
       AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
@@ -19,78 +30,95 @@ class ApiService {
     }
   }
 
-  Future<Map<String, dynamic>> getAnsweredQuestions() async {
-    final response = await http.post(
-      Uri.parse('$baseUrl/answered-questions'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'user_id': await getDeviceId(),
-      }),
+  Future<Map<int, String>> getAnsweredQuestions() async {
+    final userId = await getDeviceId();
+    final response = await http.get(
+      Uri.parse('$baseUrl/answered-questions?user_id=$userId'),
+      headers: {'accept': 'application/json'},
     );
+
     if (response.statusCode == 200) {
-      return jsonDecode(response.body);
+      var jsonResponse = jsonDecode(response.body);
+      if (jsonResponse is! Map) {
+        throw Exception('Response is not a map');
+      }
+
+      return jsonResponse.map((key, value) =>
+          MapEntry<int, String>(int.parse(key), value.toString()));
     } else {
-      throw Exception('Failed to load answered questions');
+      throw Exception('Failure with status code ${response.statusCode}');
     }
   }
 
   Future<int> getUsedQuestionsCount() async {
-    final response = await http.post(
-      Uri.parse('$baseUrl/questions-count'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'user_id': await getDeviceId(),
-      }),
+    final userId = await getDeviceId();
+    final response = await http.get(
+      Uri.parse('$baseUrl/used-questions-count?user_id=$userId'),
+      headers: {'accept': 'application/json'},
     );
     if (response.statusCode == 200) {
-      return int.parse(response.body);
+      var jsonResponse = jsonDecode(response.body);
+      return jsonResponse['used_questions_count'];
     } else {
-      throw Exception('Failed to load questions count');
+      throw Exception('Failure with status code ${response.statusCode}');
     }
   }
 
   Future<bool> isUserPremium() async {
-    final response = await http.post(
-      Uri.parse('$baseUrl/user-premium-status'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'user_id': await getDeviceId(),
-      }),
+    final userId = await getDeviceId();
+    final response = await http.get(
+      Uri.parse('$baseUrl/is-user-premium?user_id=$userId'),
+      headers: {'accept': 'application/json'},
     );
     if (response.statusCode == 200) {
-      return response.body.toLowerCase() == 'true';
+      var jsonResponse = jsonDecode(response.body);
+      return jsonResponse['is_premium'];
     } else {
-      throw Exception('Failed to check premium status');
+      throw Exception('Failure with status code ${response.statusCode}');
     }
   }
 
-  Future<String> uploadAndGetTranscription(dynamic audioFile) async {
+  Future<String> uploadAndGetTranscription(
+      String question, dynamic audioFile) async {
     var request =
         http.MultipartRequest('POST', Uri.parse('$baseUrl/upload-audio'));
+
+    // Add audio file to the request
     request.files.add(http.MultipartFile.fromBytes('audio', audioFile,
-        filename: 'audio-file'));
+        filename: 'audio.webm'));
+
+    // Add user_id and question to the request fields
     request.fields['user_id'] = await getDeviceId();
-    var response = await request.send();
-    if (response.statusCode == 200) {
-      return await response.stream.bytesToString();
+    request.fields['question'] = question;
+
+    var streamedResponse = await request.send();
+    if (streamedResponse.statusCode == 200) {
+      var responseBody = await streamedResponse.stream.bytesToString();
+      var jsonResponse = jsonDecode(responseBody);
+      return jsonResponse['transcription'];
     } else {
-      throw Exception('Failed to upload audio and get transcription');
+      throw Exception(
+          'Failure with status code ${streamedResponse.statusCode}');
     }
   }
 
-  Future<Map<String, dynamic>> getAIFeedback(String transcription) async {
+  Future<String> submitTranscription(
+      int questionId, String question, String transcription) async {
     final response = await http.post(
-      Uri.parse('$baseUrl/ai-feedback'),
+      Uri.parse('$baseUrl/submit'),
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode({
         'transcription': transcription,
         'user_id': await getDeviceId(),
+        'question': question,
+        'question_id': questionId,
       }),
     );
     if (response.statusCode == 200) {
-      return jsonDecode(response.body);
+      var jsonResponse = jsonDecode(response.body);
+      return jsonResponse['evaluation'];
     } else {
-      throw Exception('Failed to get AI feedback');
+      throw Exception('Failure with status code ${response.statusCode}');
     }
   }
 }
